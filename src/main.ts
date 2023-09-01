fa7a2ecc52610dc2b93b3607aaf4f5646e6bd622
Jaeha Lee <wogkdkrm112@gmail.com>
2023-09-01 16:40:18 +0900

    feat: graphql 서버 생성

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const app = express();
 
+const schema = buildSchema(`
+  type Query {
+    description: String
+  }
+`);
+
+const root = {
+  description: 'hello world',
+};
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
 const PORT = 4000;
 app.listen(PORT, async () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}/graphql`);
 });
