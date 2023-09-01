91c4be872fb6ef4c35124203dbb026a3d9599813
Jaeha Lee <wogkdkrm112@gmail.com>
2023-09-01 19:24:24 +0900

    feat: graphql model, tools 분리

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,25 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { loadFilesSync } from '@graphql-tools/load-files';
+
+import graphqlPostsModel from './posts/posts.model';
+import graphqlCommentsModel from './comments/comments.model';
 
 const app = express();
 
-const schema = buildSchema(`
-  type Query {
-    posts: [Post]
-    comments: [Comment]
-  }
+// .graphql로 끝나는 모든파일 불러오기
+const loadedTypes = loadFilesSync('**/*', {
+  extensions: ['graphql'],
+});
 
-  type Post {
-    id: ID!
-    title: String!
-    description: String!
-    comments: [Comment]
-  }
-
-  type Comment {
-    id: ID!
-    text: String!
-    likes: Int
-  }
-`);
+const schema = makeExecutableSchema({
+  typeDefs: loadedTypes,
+});
 
 const root = {
-  posts: [
-    {
-      id: 'post1',
-      title: 'first post',
-      description: 'first post description',
-      comments: [
-        {
-          id: 'comment1',
-          text: 'first comment',
-          likes: 1,
-        },
-      ],
-    },
-    {
-      id: 'post2',
-      title: 'second post',
-      description: 'second post description',
-      comments: [],
-    },
-  ],
-  comments: [
-    {
-      id: 'comment1',
-      text: 'first comment',
-      likes: 1,
-    },
-  ],
+  posts: graphqlPostsModel,
+  comments: graphqlCommentsModel,
 };
 app.use(
   '/graphql',
